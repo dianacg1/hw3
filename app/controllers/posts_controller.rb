c1be4f12e3a87f54d68bdb79a1d575ec9d09350e
dianacg1 <diana.carreno@kellogg.northwestern.edu>
2022-02-23 05:25:20 +0000

    new post added to mexico

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,22 @@
+class PostsController < ApplicationController
+
+    def index
+        @posts = Post.all
+      end
+    
+      def show
+        @post = Post.find(params["id"])
+      end
+    
+      def new
+        @post = Post.new
+        @post.place_id = params["place_id"]
+      end
+    
+      def create
+        @post = Post.new(params["post"])
+        @post.save
+        redirect_to "/places"
+      end
+
+end
